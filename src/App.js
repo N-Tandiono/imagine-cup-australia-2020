23b22f60a234d1e65953f68dbf7b0cb8d7fcdf25
Nick <n.tandiono@yahoo.com>
2020-12-13 01:06:12 +1100

    forms

--- src/App.js
+++ src/App.js
@@ -9,15 +9,17 @@ function App() {
 	const [change, setChange] = useState(0)
 	const [danger, setDanger] = useState(0)
 	const [success, setSuccess] = useState(0)
+	const [standardDrinks, setStandardDrinks] = useState(0)
 	const checkData = key => {
     	const data = localStorage.getItem(key);
     	if (!data) {
 			console.log('Setting default')
 			localStorage.setItem('age', 0);
+			localStorage.setItem('driving', 'no');
 			localStorage.setItem('gender', 100);
 			localStorage.setItem('weight', 60);
-			localStorage.setItem('current-danger', 90);
-			localStorage.setItem('current-warning', 20);
+			localStorage.setItem('current-danger', 80);
+			localStorage.setItem('current-warning', 10);
 			// Bug when it hits over 100
 		}
 	}
@@ -33,26 +35,98 @@ function App() {
 		let current_danger = localStorage.getItem('current-danger');
 		let current_warning = localStorage.getItem('current-warning');
 		console.log(current_warning)
-		checkData()
+		checkData('current-danger')
 		setChange(current_warning)
 		setDanger(current_danger)
 		setSuccess(100 - current_warning - current_danger)
+		checkStandardDrinks()
 		let sum = current_warning + current_danger
-		checkWarning(sum)
+		// checkWarning(sum)
 	}, [])
+
+	function changeAge(event) {
+		localStorage.setItem('age', event.target.value);
+	}
+	function changeWeight(event) {
+		localStorage.setItem('weight', event.target.value);
+	}
+	function changeGender(event) {
+		localStorage.setItem('gender', event.target.value);
+	}
+	function changeDriving(event) {
+		localStorage.setItem('driving', event.target.value);
+	}
+
+	function checkStandardDrinks() {
+		let age = localStorage.getItem('age');
+		let weight = localStorage.getItem('weight');
+		let gender = localStorage.getItem('gender');
+		let driving = localStorage.getItem('driving');
+
+		if (age <= 18) {
+			setStandardDrinks(0)
+		}
+		else {
+			if (gender == 'Male' && driving == 'Yes') {
+				setStandardDrinks(2)
+			}
+			else if (gender == 'Male' && driving == 'No') {
+				setStandardDrinks(4)
+			}
+			if (gender == 'Female' && driving == 'Yes') {
+				setStandardDrinks(2)
+			}
+			else if (gender == 'Female' && driving == 'No') {
+				setStandardDrinks(4)
+			}
+		}
+	}
 
   	return (
     <>
     <div className="title">
       Alcohmeter
     </div>
-	<ProgressBar>
-		<ProgressBar striped variant="danger" now={danger} key={1} />
-		<ProgressBar variant="warning" now={change} key={2} />
-		<ProgressBar striped variant="success" now={success} key={3} />
-	</ProgressBar>
-	<img src='https://media.discordapp.net/attachments/787116842300211231/787283704678055976/0c5ff60d5d8498db4dd281254bc03449.jpg?width=587&height=663'></img>
-    </>
+	<div className="body">
+		<form>
+			<h6>Update Information:</h6>
+			<label>
+				Age: 
+				<input type="text" name="age" onChange={changeAge.bind(this)}/>
+			</label>
+			<br/>
+			<label>
+				Weight:
+				<input type="text" name="weight" onChange={changeWeight.bind(this)}/>
+			</label>
+			<br/>
+			<label>
+				Gender:
+				<br/>
+				<input type="radio" value="Male" name="gender" onClick={changeGender.bind(this)}/> Male
+				<br/>
+        		<input type="radio" value="Female" name="gender" onClick={changeGender.bind(this)} /> Female
+				<br/>
+				<input type="radio" value="Other" name="gender" onClick={changeGender.bind(this)} /> Other
+			</label>
+			<p>Will you be driving?</p>
+			<input type="radio" value="Yes" name="driving" onClick={changeDriving.bind(this)}/> Yes
+			<br/>
+			<input type="radio" value="No" name="driving" onClick={changeDriving.bind(this)}/> No
+		</form>
+		<h1>Current Information:</h1>
+		<h3>All information on drinking limits is measured from these statistics</h3>
+		<p>Hello {localStorage.getItem('age')}</p>
+		<p>Hello {localStorage.getItem('gender')}</p>
+		<p>Permitted Standard Drinks: {standardDrinks}</p>
+      	<ProgressBar>
+			<ProgressBar striped variant="danger" now={danger} key={1} />
+			<ProgressBar variant="warning" now={change} key={2} />
+			<ProgressBar striped variant="success" now={success} key={3} />
+		</ProgressBar>
+		<img src='https://media.discordapp.net/attachments/787116842300211231/787283704678055976/0c5ff60d5d8498db4dd281254bc03449.jpg?width=587&height=663'></img>
+    </div>
+	</>
   );
 }
 
